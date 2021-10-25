1fef2f8e420a7744e853335661f145843bb20dc4
Archietector0 <avatar10.01@bk.ru>
2021-10-26 03:45:51 +0400

    fin_ex00

---
+++ cpp07/ex00/MyFuncs.hpp
@@ -0,0 +1,32 @@
+#ifndef MYFUNCS_HPP
+#define MYFUNCS_HPP
+
+#include <string>
+#include <iostream>
+
+template <typename TSwap>
+void    swap( TSwap &a, TSwap &b ) {
+    TSwap buff;
+
+    buff = a;
+    a = b;
+    b = buff;
+}
+
+template <typename TMin>
+TMin    min( TMin a, TMin b ) {
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+template <typename TMax>
+TMax    max( TMax a, TMax b ) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+#endif
